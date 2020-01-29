306e8b66fb7e1600044b24f0c1309be1d1a61d31
Benjamin Freeman <benjamin.freeman@irit.fr>
2020-01-29 17:23:54 +0100

    Change to download latest version of VS extensions

--- download_plugins.sh
+++ download_plugins.sh
@@ -3,8 +3,8 @@
 if [ ! -d  "plugins" ];
 then
     mkdir plugins
-if
+fi
 
 cd plugins && \
-curl https://marketplace.visualstudio.com/_apis/public/gallery/publishers/marus25/vsextensions/cortex-debug/0.3.4/vspackage -O -J --compressed && \
-curl https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/python/2019.11.50794/vspackage -O -J --compressed
+curl https://marketplace.visualstudio.com/_apis/public/gallery/publishers/marus25/vsextensions/cortex-debug/latest/vspackage -O -J --compressed && \
+curl https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/python/latest/vspackage -O -J --compressed
